7446416f093b3060ab24d23c3b9ee9a1ace98b83
sexyboyjayden <dev@matchu.vip>
2024-12-08 18:29:40 +0900

    refactor: HttpREquest body 로직 추가 및 테스트 코드 추가

--- src/main/java/http/HttpRequest.java
+++ src/main/java/http/HttpRequest.java
@@ -3,6 +3,7 @@ package http;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import util.HttpRequestUtils;
+import util.IOUtils;
 
 import java.io.BufferedReader;
 import java.io.InputStream;
@@ -20,50 +21,50 @@ public class HttpRequest {
     private String url;
     private Map<String, String> headers = new HashMap<String, String>();
     private Map<String, String> params = new HashMap<String, String>();
+    private Map<String, String> body = new HashMap<String, String>();
 
 
     public HttpRequest(InputStream in) {
         try {
             this.in = in;
-            this.br = new BufferedReader(new InputStreamReader(this.in));
+            this.br = new BufferedReader(new InputStreamReader(this.in, "UTF-8"));
 
             String line = br.readLine();
+            if (line == null) return;
+
             String[] tokens = line.split(" ");
             this.method = tokens[0];
             this.url = tokens[1];
             setParameter(this.url);
-            this.setHeader();
+
+            line = br.readLine();
+
+            while(line != null && !line.isEmpty()) {
+                String[] headerKeyValue = line.split(":", 2);
+                headers.put(headerKeyValue[0].trim(), headerKeyValue[1].trim());
+                line = br.readLine();
+            }
+            if ((this.getMethod()).equals("POST")) {
+                this.setBody();
+            }
         } catch (Exception e) {
             log.error(e.getMessage(), e);
         }
     }
 
-    private void setHeader(){
-        String line;
-        try {
-            while((line = br.readLine()) != null && !line.isEmpty()) {
-                if (line == null) return;
-                String[] headerKeyValue = line.split(":", 2);
-                headers.put(headerKeyValue[0], headerKeyValue[1].trim());
-            }
-        }catch (Exception e){
-            log.error(e.getMessage(), e);
-        }
+    private void setBody() throws Exception {
+        String contentLength = headers.get("Content-Length");
+        String data = IOUtils.readData(br, Integer.parseInt(contentLength));
+        body = HttpRequestUtils.parseQueryString(data);
     }
 
     private void setParameter(String url){
-        String params = url.split(Pattern.quote("?"))[1];
-        this.params = HttpRequestUtils.parseQueryString(params);
-    }
-
-    public void showLog(){
-        try {
-            String line = br.readLine();
-            log.debug("line {}", line);
-        } catch (Exception e) {
-            log.error(e.getMessage());
+        String[] params = url.split(Pattern.quote("?"));
+        if (params.length == 2) {
+            this.params = HttpRequestUtils.parseQueryString(params[1]);
         }
     }
+
     public String getMethod() {
         return this.method;
     }
@@ -76,5 +77,9 @@ public class HttpRequest {
 
     public Map<String, String> getParams() {
         return this.params;
+    }
+
+    public Map<String, String> getBody() {
+        return this.body;
     }
 }
--- src/test/java/http/HttpRequestTest.java
+++ src/test/java/http/HttpRequestTest.java
@@ -2,9 +2,12 @@ package http;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
@@ -13,47 +16,42 @@ import static org.junit.Assert.*;
 
 public class HttpRequestTest {
 
+    private static final Logger log = LoggerFactory.getLogger(HttpRequestTest.class);
     private String testFilePath = "src/test/resources";
 
     private HttpRequest request;
 
-    @Before
-    public void setUp() throws Exception {
-        InputStream in = new FileInputStream(new File(testFilePath + "/Http_GET.txt"));
-        this.request = new HttpRequest(in);
+
+    @Test
+    public void testGetMethod() throws IOException {
+        HttpRequest request1 = new HttpRequest(getInputStream("/Http_GET.txt"));
+        assertEquals("GET", request1.getMethod());
     }
 
     @Test
-    public void testShowLog() {
-        request.showLog();
+    public void testGetUrl() throws IOException {
+        HttpRequest request1 = new HttpRequest(getInputStream("/Http_GET.txt"));
+        assertEquals("/user/create?userId=javajigi&password=password&name=jaemin", request1.getUrl());
     }
 
     @Test
-    public void testGetMethod(){
-        assertEquals("GET", request.getMethod());
-    }
-
-    @Test
-    public void testGetUrl(){
-        assertEquals("/user/create?userId=javajigi&password=passwrod&name=jaemin", request.getUrl());
-    }
-
-    @Test
-    public void testGetheader(){
+    public void testGetheader() throws IOException {
+        HttpRequest request1 = new HttpRequest(getInputStream("/Http_GET.txt"));
         Map<String, String> expectedHeaders = new HashMap<>();
         expectedHeaders.put("HOST", "localhost:8080");
         expectedHeaders.put("Connection", "keep-alive");
         expectedHeaders.put("Accept", "*/*");
-        assertEquals(expectedHeaders, request.getHeaders());
+        assertEquals(expectedHeaders, request1.getHeaders());
     }
 
     @Test
-    public void testGetHeaderAccept() {
-        assertEquals("localhost:8080", request.getHeaders().get("HOST"));
+    public void testGetHeaderAccept() throws IOException {
+        HttpRequest request1 = new HttpRequest(getInputStream("/Http_GET.txt"));
+        assertEquals("localhost:8080", request1.getHeaders().get("HOST"));
     }
 
     @Test
-    public void testGetParamter(){
+    public void testGetParamter() throws IOException {
         Map<String, String> expectedParams = new HashMap<>();
         expectedParams.put("userId", "javajigi");
         expectedParams.put("password", "password");
@@ -64,5 +62,21 @@ public class HttpRequestTest {
     @Test
     public void testGetParamterUserId(){
         assertEquals("javajigi", request.getParams().get("userId"));
+    }
+
+    @Test
+    public void testPOSTBody() throws IOException {
+        HttpRequest request1 = new HttpRequest(getInputStream("/Http_POST.txt"));
+        Map<String, String> expectedParams = new HashMap<>();
+        expectedParams.put("userId", "minseok");
+        expectedParams.put("password", "password");
+        expectedParams.put("name", "minseok");
+        Map<String,String> body = request1.getBody();
+        assertEquals(expectedParams, body);
+    }
+
+    private InputStream getInputStream(String path) throws IOException {
+        log.debug("file path. {}", path);
+        return new FileInputStream(new File(testFilePath + path));
     }
 }
