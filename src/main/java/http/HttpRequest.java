fb758aff5aae5663a71be993c08413b231fda300
sexyboyjayden <dev@matchu.vip>
2024-12-08 20:23:39 +0900

    refactor: RequestLine class 추가

--- src/main/java/http/HttpRequest.java
+++ src/main/java/http/HttpRequest.java
@@ -8,7 +8,6 @@ import util.IOUtils;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.regex.Pattern;
@@ -17,26 +16,18 @@ public class HttpRequest {
     private static final Logger log = LoggerFactory.getLogger(HttpRequest.class);
     private InputStream in;
     private BufferedReader br;
-    private String method;
-    private String url;
     private Map<String, String> headers = new HashMap<String, String>();
-    private Map<String, String> params = new HashMap<String, String>();
     private Map<String, String> body = new HashMap<String, String>();
+    private RequestLine requestLine;
 
 
     public HttpRequest(InputStream in) {
         try {
             this.in = in;
             this.br = new BufferedReader(new InputStreamReader(this.in, "UTF-8"));
-
             String line = br.readLine();
-            log.debug("line is. {}", line);
             if (line == null) return;
-
-            String[] tokens = line.split(" ");
-            this.method = tokens[0];
-            this.url = tokens[1];
-            setParameter(this.url);
+            this.requestLine = new RequestLine(line);
 
             line = br.readLine();
 
@@ -59,25 +50,20 @@ public class HttpRequest {
         body = HttpRequestUtils.parseQueryString(data);
     }
 
-    private void setParameter(String url){
-        String[] params = url.split(Pattern.quote("?"));
-        if (params.length == 2) {
-            this.params = HttpRequestUtils.parseQueryString(params[1]);
-        }
-    }
+
 
     public String getMethod() {
-        return this.method;
+        return requestLine.getMethod();
     }
     public String getUrl() {
-        return this.url;
+        return requestLine.getUrl();
     }
     public Map<String, String> getHeaders() {
         return this.headers;
     }
 
     public Map<String, String> getParams() {
-        return this.params;
+        return requestLine.getParams();
     }
 
     public Map<String, String> getBody() {
