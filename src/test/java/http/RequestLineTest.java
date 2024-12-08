d65b4dbdfa5db7bb1088892271d21d116ee2416a
sexyboyjayden <dev@matchu.vip>
2024-12-08 20:26:09 +0900

    refactor: RequestLine 테스트 코드 구착

--- src/test/java/http/RequestLineTest.java
+++ src/test/java/http/RequestLineTest.java
@@ -1,7 +1,36 @@
 package http;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.junit.Assert.*;
 
 public class RequestLineTest {
 
+    private RequestLine requestLine;
+    @Before
+    public void setUp() throws Exception {
+        requestLine = new RequestLine("GET /index.html HTTP/1.1");
+    }
+
+    @Test
+    public void getMethod() {
+        assertEquals("GET", requestLine.getMethod());
+    }
+
+    @Test
+    public void getUrl() {
+        assertEquals("/index.html", requestLine.getUrl());
+    }
+
+    @Test
+    public void getParams() {
+        RequestLine requestLine = new RequestLine("GET /user/create?username=hello HTTP/1.1");
+        Map<String, String> expectedParams = new HashMap<>();
+        expectedParams.put("username", "hello");
+        assertEquals(expectedParams, requestLine.getParams());
+    }
 }
